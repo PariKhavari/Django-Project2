1cbadea8799b1a5fc511e5fcfbfba122964527e8
pari khavari <khavaripari60@gmail.com>
2025-10-23 14:46:57 +0200

    basig views nnd urls angepasst

--- fruit_app2/views.py
+++ fruit_app2/views.py
@@ -1,3 +1,16 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+import json
+
 
 # Create your views here.
+
+def send_fruites(request):
+    fruits = [
+        {"name": "Apple",  "weight": 200, "color": "red"},
+        {"name": "Banana", "weight": 120, "color": "yellow"},
+        {"name": "Grape",  "weight": 5,   "color": "purple"},
+        {"name": "Pear",   "weight": 220, "color": "green"},
+        {"name": "Orange", "weight": 250, "color": "orange"},
+    ]
+    return JsonResponse(fruits, safe=False)
